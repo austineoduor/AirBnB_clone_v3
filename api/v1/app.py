f0a11f3824f16a6b820cd062c3bbeb4b376fa69f
joseph <jsphmariga@gmail.com>
2022-10-24 23:34:15 +0000

    Add 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """App file"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import abort, Flask, jsonify, make_response
 from models import storage
 from os import getenv
 
@@ -20,6 +20,12 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """ calls """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    response_error = {"error": "Not found"}
+    return make_response(jsonify(response_error), 404)
 
 
 if __name__ == '__main__':
