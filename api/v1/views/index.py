9d9dd9d7047bc0c1a42f930e4d113a279abb3f5b
joseph <jsphmariga@gmail.com>
2022-10-24 23:11:52 +0000

    Add stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,25 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     response = {"status": "OK"}
+    return jsonify(response)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    response = {}
+    checkers = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+            }
+    for key, val in checkers.items():
+        response[val] = storage.count(key)
     return jsonify(response)
