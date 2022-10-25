16268717488876df5adbc28cb1f9ad5677db92e2
austineoduor <austineoduor57@gmail.com>
2022-10-25 17:11:35 +0300

    file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python3
-"""App file"""
-from api.v1.views import app_views
-from flask import abort, Flask, render_template, jsonify, make_response
-from flask_cors import CORS
+""" Flask Application """
 from models import storage
-from os import getenv
+from api.v1.views import app_views
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
-
-"""app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True"""
 app = Flask(__name__)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def close_db(error):
     """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_404(error):
-    """ 
-    404 Error
+def not_found(error):
+    """ 404 Error
+    ---
     responses:
       404:
         description: a resource was not found
     """
-    response_error = {"error": "Not found"}
-    return make_response(jsonify(response_error), 404)
+    return make_response(jsonify({'error': "Not found"}), 404)
 
-
-"""app.config['SWAGGER'] = {
-
-
+app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
     'uiversion': 3
 }
 
-Swagger(app)"""
+Swagger(app)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    """ Main Function """
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
     if not host:
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,53 +1,92 @@
 #!/usr/bin/python3
-"""State views"""
+""" objects that handle all default RestFul API actions for States """
+from models.state import State
+from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from models import storage, classes
+from flasgger.utils import swag_from
 
 
-@app_views.route('/states/', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def states():
-    if request.method == 'GET':
-        lst = []
-        objs = storage.all('State')
-        for key, val in objs.items():
-            lst.append(objs[key].to_dict())
-        return jsonify(lst)
-
-    elif request.method == 'POST':
-        if not request.get_json():
-            abort(400, 'Not a JSON')
-        if not request.get_json("name"):
-            abort(400, 'Missing name')
-        State = classes.get("State")
-        obj = State(request.get_json())
-        obj.save()
-        return jsonify(obj.to_dict()), 201
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_state.yml', methods=['GET'])
+def get_states():
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
 
-@app_views.route('/states/<state_id>',
-                 methods=['GET', 'DELETE', 'POST', 'PUT'])
-def states_id(state_id):
-    obj = storage.get('State', state_id)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_id_state.yml', methods=['get'])
+def get_state(state_id):
+    """ Retrieves a specific State """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
 
-    if request.method == 'GET':
-        try:
-            print(obj)
-            return jsonify(obj.to_dict())
-        except Exception:
-            abort(404)
+    return jsonify(state.to_dict())
 
-    if request.method == 'DELETE':
-        try:
-            storage.delete(obj)
-            return jsonify({}), 200
-        except Exception as e:
-            return e
 
-    if request.method == 'PUT':
-        if not request.get_json():
-            abort(400, 'Not a JSON')
-        body = request.get_json()
-        for key, value in body.items():
-            setattr(obj, key, value)
-        return jsonify(obj.to_dict())
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
+
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/state/post_state.yml', methods=['POST'])
+def post_state():
+    """
+    Creates a State
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    data = request.get_json()
+    instance = State(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/state/put_state.yml', methods=['PUT'])
+def put_state(state_id):
+    """
+    Updates a State
+    """
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
