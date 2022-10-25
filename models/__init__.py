858db52f6c42d4a12ac8f58555faeefab18c7722
joseph <jsphmariga@gmail.com>
2022-10-25 01:28:58 +0000

    Update storage adn classes

--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,11 @@ from os import getenv
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
+    from models.engine import db_storage
+    classes = db_storage.classes
+    storage = db_storage.DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+    from models.engine import file_storage
+    classes = file_storage.classes
+    storage = file_storage.FileStorage()
 storage.reload()
