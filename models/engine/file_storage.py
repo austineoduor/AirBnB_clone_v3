03e406fb0d042e2e60eb0ad315fee1096570ee9e
austineoduor <austineoduor57@gmail.com>
2022-10-26 14:08:49 +0300

    file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -78,9 +78,11 @@ class FileStorage:
         id: string representing the object ID
         Returns the obj based on the class and its ID, or None if not found
         """
-        objs = self.all(cls)
-        obj = "{}.{}".format(cls.__name__, id)
-        return objs[obj]
+         if cls and id:
+            fetch_obj = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(fetch_obj)
+        return None
 
 
     def count(self, cls=None):
