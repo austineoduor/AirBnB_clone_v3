29784d39d0c6561880d9e4c0396091906d5c7aa0
austineoduor <austineoduor57@gmail.com>
2022-10-26 15:55:50 +0300

    file

--- tests/test_console.py
+++ tests/test_console.py
@@ -25,7 +25,7 @@ class TestConsoleDocs(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
     """def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
+        Test for the console.py module docstring
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
@@ -37,4 +37,4 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
-                        
+       
