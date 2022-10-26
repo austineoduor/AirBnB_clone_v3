a744c193d0be359f86406e52afbe9b93d1e78edd
austineoduor <austineoduor57@gmail.com>
2022-10-26 15:05:24 +0300

    file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -87,7 +87,6 @@ class FileStorage:
             return (len(self.all(cls)))
         return (len(self.all()))
 
-
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
